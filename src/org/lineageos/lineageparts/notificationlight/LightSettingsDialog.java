LineageOS/android_packages_apps_LineageParts

LightSettingsDialog: add bundle extras for preview color and duration
The sdk now accepts an abstract way to force preview values.

Change-Id: I8ca686ab455e19b928b277585c46099c3ac6daf9
Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

src/org/lineageos/lineageparts/notificationlight/LightSettingsDialog.java
@@ -318,6 +318,9 @@ public class LightSettingsDialog extends AlertDialog implements
         if  (mLedBrightness > 0 && mLedBrightness < LedValues.LIGHT_BRIGHTNESS_MAXIMUM) {
             b.putInt(LineageNotification.EXTRA_FORCE_LIGHT_BRIGHTNESS, mLedBrightness);
         }
+        b.putInt(LineageNotification.EXTRA_FORCE_PREVIEW_COLOR, color);
+        b.putInt(LineageNotification.EXTRA_FORCE_PREVIEW_LIGHT_ON_MS, speedOn);
+        b.putInt(LineageNotification.EXTRA_FORCE_PREVIEW_LIGHT_OFF_MS, speedOff);
 
         createNotificationChannel();
 

AOKP/packages_apps_LineageParts

src/org/lineageos/lineageparts/notificationlight/LightSettingsDialog.java
@@ -318,6 +318,9 @@ public class LightSettingsDialog extends AlertDialog implements
         if  (mLedBrightness > 0 && mLedBrightness < LedValues.LIGHT_BRIGHTNESS_MAXIMUM) {
             b.putInt(LineageNotification.EXTRA_FORCE_LIGHT_BRIGHTNESS, mLedBrightness);
         }
+        b.putInt(LineageNotification.EXTRA_FORCE_COLOR, color);
+        b.putInt(LineageNotification.EXTRA_FORCE_LIGHT_ON_MS, speedOn);
+        b.putInt(LineageNotification.EXTRA_FORCE_LIGHT_OFF_MS, speedOff);
 
         createNotificationChannel();
 